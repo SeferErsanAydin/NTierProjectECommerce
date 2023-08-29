d55902ee04200466fbfdaffc18615dce6d072503
Sefer Ersan Aydin <ersanaydin@outlook.com>
2023-08-29 10:06:53 +0300

    ShoppingController updated

--- Project.MVCUI/Controllers/ShoppingController.cs
+++ Project.MVCUI/Controllers/ShoppingController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using PagedList;
+using Project.BLL.DesignPatterns.GenericRepository.ConcRep;
+using Project.MVCUI.VMClasses;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,28 @@ namespace Project.MVCUI.Controllers
 {
     public class ShoppingController : Controller
     {
-        // GET: Shopping
-        public ActionResult ShoppingList()
+        OrderRepository _oRep;
+        ProductRepository _pRep;
+        CategoryRepository _cRep;
+        OrderDetailRepository _odRep;
+
+        public ShoppingController()
         {
-            return View();
+            _oRep = new OrderRepository();
+            _pRep = new ProductRepository();
+            _cRep = new CategoryRepository();
+            _odRep = new OrderDetailRepository();
+        }
+        public ActionResult ShoppingList(int? page, int? categoryID) //nulleable ints will work with the Pagination/PagedList and allow user to select shopping page
+        {
+            PaginationVM pavm = new PaginationVM
+            {
+                PagedProducts = categoryID == null ? _pRep.GetActives().ToPagedList(page ?? 1, 9) : _pRep.Where(x => x.CategoryID == categoryID).ToPagedList(page ?? 1, 9),
+                Categories = _cRep.GetActives()
+            };
+
+            if (categoryID != null) TempData["catID"] = categoryID;
+            return View(pavm);
         }
     }
 }
